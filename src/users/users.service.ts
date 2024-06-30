e5dc725341b4a6ada148532b4b96ecde71dd9f2b
danstonebuilt <dnansel.dev@gmail.com>
2024-06-29 21:55:46 -0300

    feat(general): added improvements

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './users.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectRepository(User) private rep: Repository<User>) {}
+
+  create(email: string, password: string) {
+    const user = this.rep.create({ email, password });
+
+    return this.rep.save(user);
+  }
+}
